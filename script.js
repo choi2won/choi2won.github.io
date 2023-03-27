2a2d2c994aa884b79d25936ef3f166063c6879f5
choi2won <choi2won@gmail.com>
2023-03-27 23:20:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,17 @@
-#calculator {
-  width: 300px;
-  margin: 0 auto;
-  border: 1px solid black;
-  border-radius: 5px;
-  padding: 10px;
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+const display = document.getElementById("display");
+
+function clearDisplay() {
+  display.value = "0";
 }
 
-#display {
-  width: 100%;
-  margin-bottom: 10px;
-  font-size: 24px;
-  text-align: right;
-  border: none;
-  background-color: #f1f1f1;
-  padding: 10px;
+function addValue(value) {
+  if (display.value === "0") {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
 }
 
-.button {
-  width: 50px;
-  height: 50px;
-  border-radius: 5px;
-  margin: 5px;
-  font-size: 20px;
-  cursor: pointer;
-}
-
-.operator {
-  background-color: #f44336;
-  color: white;
-}
-
-.clear {
-  background-color: #4CAF50;
-  color: white;
-}
-
-.sqrt {
-  background-color: #FFA500;
-  color: white;
-}
-
-.sin {
-  background-color: #1E90FF;
-  color: white;
-}
-
-.cos {
-  background-color: #008000;
-  color: white;
-}
-
-.ad {
-  margin: 20px 0;
-  text-align: center;
-}
-
-.footer {
-  text-align: center;
-  margin-top: 20px;
+function calculate() {
+  display.value = eval(display.value);
 }
